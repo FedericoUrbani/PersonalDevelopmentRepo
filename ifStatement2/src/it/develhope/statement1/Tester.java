a9fe5187e51b28de249e6206c8743026851df00d
Federico Urbani <urbanifederico5@gmail.com>
2023-01-23 01:43:24 +0100

    ifStatementUpdate

---
+++ ifStatement2/src/it/develhope/statement1/Tester.java
@@ -0,0 +1,13 @@
+package it.develhope.statement1;
+
+public class Tester {
+    public static void main(String[] args) {
+
+        String surname="Urbani";
+        if(surname.contains("ni")){
+            System.out.println("your surname contain the sequence of letters 'ni'");
+        }else{
+            System.out.println("your name doesn't contain the sequence of letters 'ni'");
+        }
+    }
+};
---
+++ ifStatement4/src/it/develhope/ifstatement04/Main.java
@@ -0,0 +1,8 @@
+package it.develhope.ifstatement04;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person=new Person();
+        System.out.println(person.getLifeStage(person.age));
+    }
+}
